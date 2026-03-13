b87754bc9f46cb0ae34ff1d73767c308cdb46e8e
Jael <Jael.ruvalcaba>
2026-03-12 21:02:12 -0700

    add chess person

--- src/features/home/components/Atletas/Model/AtletasModel.ts
+++ src/features/home/components/Atletas/Model/AtletasModel.ts
@@ -14,6 +14,7 @@ export const ATLETAS_DATA = {
         { name: "Kimberly Becerrra", sport: "Gimnasia Artística", image: "/images/kimberly_becerra.jpeg" },
         { name: "Renata Ramirez", sport: "Gimnasia Artística", image: "/images/renata_ramirez_nuñes.jpeg" },
         { name: "Mateo Ruiz", sport: "Ajedrez", image: "/images/mateo_ruiz_gastelum.jpeg" },
+        { name: "Roberto de Jesus", sport: "Ajedrez", image: "/images/roberto_de_jesus.jpeg" },
         { name: "Campeonas CED", sport: "Demás Atletas", image: "/images/campeonas_ced.jpg" }
     ]
 };
